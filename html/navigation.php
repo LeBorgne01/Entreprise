cfcfdc546722cf33e937e04d7d0937c5456c60fb
BREVET MAEL <mael.brevet@gmail.com>
2018-03-12 09:59:21 +0100

    trad

--- html/navigation.php
+++ html/navigation.php
@@ -1,29 +1,27 @@
+<div id="conteneur">
+    <div id="logo">
 
-        <div id="conteneur">
-            <div id="logo">
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="">Project</a>
+            </li>
+            <li>
+                <a href="../html/teamPage.php">Team</a>
+            </li>
+            <li>
+                <a href="../php/var_session.php">Contact</a>
+            </li>
+        </ul>
+    </nav>
 
-            </div>
-            <nav>
-
-                <ul>
-                    <li>
-                    <a href="index.php">Home</a>
-                    </li>
-                    <li>
-                        <a href="">Project</a>
-                    </li>
-                    <li>
-                        <a href="../html/teamPage.php">Team</a>
-                    </li>
-                    <li>
-                        <a href="../php/var_session.php">Contact</a>
-                    </li>
-                </ul>
-            </nav>
-
-            <div>
-                <button id="btn_french" onclick="">Français</button>
-                <button id="btn_english" onclick="">English</button>
-            </div>
-        </div>
+    <div>
+        <button id="btn_french" onclick="">Français</button>
+        <button id="btn_english" onclick="">English</button>
+    </div>
+</div>
 
